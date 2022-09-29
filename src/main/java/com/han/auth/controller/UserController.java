d87819766883d71e98ce6d75defaf1eed18e07a8
yuuki <yong.han.life@outlook.com>
2022-09-29 20:59:04 +0800

    feat: user api

--- src/main/java/com/han/auth/controller/UserController.java
+++ src/main/java/com/han/auth/controller/UserController.java
@@ -1,9 +1,30 @@
 package com.han.auth.controller;
 
 
+import com.han.auth.base.RestResponse;
+import com.han.auth.base.annotation.CurrentUser;
+import com.han.auth.entity.User;
+import com.han.auth.request.user.RegisterByEmailRequest;
+import com.han.auth.response.user.RefreshTokenResponse;
+import com.han.auth.response.user.RegisterResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
+    @PostMapping("/register")
+    public RestResponse<RegisterResponse> registerByEmail(RegisterByEmailRequest request) {
+        // TODO
+        return null;
+    }
+
+    @GetMapping("/token/refresh")
+    public RestResponse<RefreshTokenResponse> refreshToken(@CurrentUser User currentUser) {
+        // TODO
+        return null;
+    }
 }
---
+++ src/main/java/com/han/auth/response/user/RefreshTokenResponse.java
@@ -0,0 +1,4 @@
+package com.han.auth.response.user;
+
+public class RefreshTokenResponse {
+}
